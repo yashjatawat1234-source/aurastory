9fc82983c8531ea5b95c8d3033a6b9063f05c3b6
Yash Jatawat <your.email@example.com>
2026-09-20 22:51:58 +0530

    Remove rolldown override and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./', import.meta.url)),
-    },
-  },
   build: {
     cssMinify: false,
     minify: 'esbuild',
